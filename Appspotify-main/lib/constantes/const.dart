b92f89090e65f4e930d6a156bfe8d1e927f6bf7a
Hugo803 <hugocuellar76@gmail.com>
2022-05-03 14:33:58 -0600

    Actividades

---
+++ Appspotify-main/lib/constantes/const.dart
@@ -0,0 +1,93 @@
+import 'package:spotifyclone/paginas/principal_app.dart';
+
+const List song_type_1 = ["OverView", "Track", "Album", "Follow"];
+
+const List song_type_2 = ["Trending this week"];
+
+const List song_type_3 = ["New Album"];
+
+const List song = [
+  {
+    "img":
+        "https://p0.pikrepo.com/preview/980/1013/silhouette-of-deer-wallpaper.jpg",
+    "title": "Ciervo",
+    "descriptions": "Ciervo maullando en un ciello estrellado",
+    "song_count": "100 canciones",
+    "date": "hace 19hr",
+    "song_url": "song/1.mp3",
+    "songs": [
+      {"title": "El churro numero 2", "duration": "1.20"},
+      {"title": "El churro numero 2", "duration": "1.20", "portada": ""}
+    ]
+  }
+];
+
+const List song2 = [
+  {
+    "img": "https://www.gratistodo.com/wp-content/uploads/2016/09/caballos.jpg",
+    "title": "Caballo",
+    "descriptions": "Espiritu Indomable",
+    "song_count": "100 canciones",
+    "date": "hace 19hr",
+    "song_url": "song/1.mp3",
+    "songs": [
+      {"title": "El churro numero 2", "duration": "1.20"},
+      {"title": "El churro numero 2", "duration": "1.20", "portada": ""}
+    ]
+  },
+  {
+    "img":
+        "https://cdn.bhdw.net/im/wolves-in-winter-forest-papel-pintado-26580_w635.jpg",
+    "title": "Lobo",
+    "descriptions": "Lobos en el bosque de invierno",
+    "song_count": "100 canciones",
+    "date": "hace 19hr",
+    "song_url": "song/1.mp3",
+    "songs": [
+      {"title": "El churro numero 2", "duration": "1.20"},
+      {"title": "El churro numero 2", "duration": "1.20", "portada": ""}
+    ]
+  },
+  {
+    "img":
+        "https://cdn.bhdw.net/im/siberian-husky-dog-papel-pintado-78610_w635.jpg",
+    "title": "Perro",
+    "descriptions": "Perro husky siberiano",
+    "song_count": "100 canciones",
+    "date": "hace 19hr",
+    "song_url": "song/1.mp3",
+    "songs": [
+      {"title": "El churro numero 2", "duration": "1.20"},
+      {"title": "El churro numero 2", "duration": "1.20", "portada": ""}
+    ]
+  },
+  {
+    "img":
+        "https://cdn.bhdw.net/im/jaguar-on-the-hunt-papel-pintado-26775_w635.jpg",
+    "title": "Jaguar",
+    "descriptions": "Jaguar en preparacion para cazar",
+    "song_count": "100 canciones",
+    "date": "hace 19hr",
+    "song_url": "song/1.mp3",
+    "songs": [
+      {"title": "El churro numero 2", "duration": "1.20"},
+      {"title": "El churro numero 2", "duration": "1.20", "portada": ""}
+    ]
+  }
+];
+
+const List song3 = [
+  {
+    "img":
+        "https://p0.pikrepo.com/preview/980/1013/silhouette-of-deer-wallpaper.jpg",
+    "title": "Ciervo",
+    "descriptions": "Ciervo maullando en un ciello estrellado",
+    "song_count": "100 canciones",
+    "date": "hace 19hr",
+    "song_url": "song/1.mp3",
+    "songs": [
+      {"title": "El churro numero 2", "duration": "1.20"},
+      {"title": "El churro numero 2", "duration": "1.20", "portada": ""}
+    ]
+  }
+];
---
+++ Appspotify-main/lib/paginas/home.dart
@@ -0,0 +1,277 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_feather_icons/flutter_feather_icons.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+
+import '../constantes/const.dart';
+
+class PaginaPrincipal extends StatefulWidget {
+  @override
+  State<PaginaPrincipal> createState() => _PaginaPrincipalState();
+}
+
+class _PaginaPrincipalState extends State<PaginaPrincipal> {
+  int activoMenu1 = 0;
+  int activoMenu2 = 0;
+  int activoMenu3 = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: mostrarAppBar(),
+      ),
+      body: bodyAppSpf(),
+      //: mostrarAppBar(),
+    );
+  }
+
+  Widget mostrarAppBar() {
+    return AppBar(
+      backgroundColor: Colors.black,
+      elevation: 0,
+      title: Padding(
+        padding: const EdgeInsets.only(left: 10, right: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Explorar",
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Icon(Entypo.list),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget bodyAppSpf() {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            //crossAxisAlignment: CrossAxisAlignment.start,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 30, top: 20),
+              child: Row(
+                  children: List.generate(song_type_1.length, (index) {
+                return Padding(
+                  padding: const EdgeInsets.only(right: 25),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        activoMenu1 = index;
+                      });
+                    },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          song_type_1[index],
+                          style: TextStyle(
+                              //color: Colors.grey,
+                              fontSize: 15,
+                              color: activoMenu1 == index
+                                  ? Colors.green
+                                  : Colors.grey,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(
+                          height: 3,
+                        ),
+                        activoMenu1 == index
+                            ? Container(
+                                width: 10,
+                                height: 3,
+                                decoration: BoxDecoration(
+                                    color: Colors.green,
+                                    borderRadius: BorderRadius.circular(5)),
+                              )
+                            : Container()
+                      ],
+                    ),
+                  ),
+                );
+              })),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 30),
+              child: Row(
+                  children: List.generate(song.length, (index) {
+                return Padding(
+                  padding: const EdgeInsets.only(right: 30),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 305,
+                          height: 180,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: NetworkImage(song[index]['img']),
+                                  fit: BoxFit.cover),
+                              color: Colors.green,
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          song[index]['title'],
+                          style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: 180,
+                          child: Text(
+                            song[index]['descriptions'],
+                            maxLines: 1,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w600),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              })),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            //crossAxisAlignment: CrossAxisAlignment.start,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 30, top: 20),
+              child: Row(
+                  children: List.generate(song_type_2.length, (index) {
+                return Padding(
+                  padding: const EdgeInsets.only(right: 25),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        activoMenu1 = index;
+                      });
+                    },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          song_type_2[index],
+                          style: TextStyle(
+                              //color: Colors.grey,
+                              fontSize: 15,
+                              color: activoMenu2 == index
+                                  ? Colors.green
+                                  : Colors.grey,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(
+                          height: 3,
+                        ),
+                        activoMenu2 == index
+                            ? Container(
+                                width: 10,
+                                height: 3,
+                                decoration: BoxDecoration(
+                                    color: Colors.green,
+                                    borderRadius: BorderRadius.circular(5)),
+                              )
+                            : Container()
+                      ],
+                    ),
+                  ),
+                );
+              })),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+
+//segundo bloque
+
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 30),
+              child: Row(
+                  children: List.generate(song2.length, (index) {
+                return Padding(
+                  padding: const EdgeInsets.only(right: 30),
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 180,
+                          height: 180,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: NetworkImage(song2[index]['img']),
+                                  fit: BoxFit.cover),
+                              color: Colors.green,
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          song2[index]['title'],
+                          style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          width: 180,
+                          child: Text(
+                            song2[index]['descriptions'],
+                            maxLines: 1,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 12,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w600),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              })),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
+    );
+  }
+}
